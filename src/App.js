baa2ab19e0f7d5cecd2deaa0ad560a782ff71322
Joseph Gu <285822850@qq.com>
2021-09-20 21:08:17 +0800

    finish the study and upgrade to latest version of api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { useLocalObservable, Observer } from "mobx-react-lite"
+
+const StoreContext = React.createContext();
+
+const StoreProvider = ({ children }) => {
+  const store = useLocalObservable(() => ({
+    bugs: ["Centipede"],
+    addBug: (bug) => {
+      store.bugs.push(bug);
+    },
+    get bugsCount() {
+      return store.bugs.length;
+    }
+  }));
+  return (
+    <StoreContext.Provider value={store}>
+      {children}
+    </StoreContext.Provider>
+  )
+}
+
+const BugsHeader = () => {
+  const store = React.useContext(StoreContext);
+  return (
+    <Observer>
+      {() => (
+        <h1>
+          {store.bugsCount} Bugs!
+        </h1>
+      )}
+    </Observer>
+
+
+  )
+}
+
+
+const BugsList = () => {
+  const store = React.useContext(StoreContext);
+
+  return (
+    <Observer>
+      {() => (
+        <ul>
+          {store.bugs.map(bug => (
+            <li key={bug}>
+              {bug}
+            </li>))}
+        </ul>
+      )}
+    </Observer>
+  )
+
+}
+
+const BugsForm = () => {
+  const store = React.useContext(StoreContext);
+  const [bug, setBug] = React.useState("")
+  return (
+    <form onSubmit={e => {
+      store.addBug(bug);
+      setBug("");
+      e.preventDefault();
+    }}>
+      <input type="text" value={bug} onChange={e => { setBug(e.target.value) }} />
+      <button type="submit">Add</button>
+    </form>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StoreProvider>
+      <main>
+        <BugsHeader />
+        <BugsList />
+        <BugsForm />
+      </main>
+    </StoreProvider>
   );
 }
 
